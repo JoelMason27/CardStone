7c6fce675f9361fd0ed2b45acc6a21b3951db418
Joel Mason <joel@cerebralfix.com>
2016-08-08 22:40:59 +1200

    * Added new main menu scene for networkmanager * Added player prefab

---
+++ Assets/Scripts/DropZone.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler {
+
+	public virtual void OnDrop(PointerEventData eventData) {
+        Debug.Log("[DropZone] OnDrop()");
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null) {
+            d.originalParent = this.transform;
+        }
+    }
+}
